d4043ae4b1cce0efa47d07bda4c77cb525f2c03e
Jonathan Tuta <jonathangetachew@gmail.com>
2019-10-23 18:31:16 -0500

    Added functionality to redirect users to home page if they're signed and try to access the sign in page.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
@@ -49,15 +49,21 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPages} />
+          <Route exact 
+            path='/signin' 
+            render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPages />)} />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null /*mapStateToProps*/, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
