def6069a54e3fc12b04f83aecf633cb71cc4781d
Jonathan Tuta <jonathangetachew@gmail.com>
2020-03-21 10:32:19 -0500

    Integrated react-persist for data persistance in the localStorage. Refactored directory and shop components by moving data to redux.

--- src/redux/root-reduer.js
+++ src/redux/root-reduer.js
@@ -1,9 +1,24 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // Get localStorage object from window setting it as default => user import { sessionStorage } (read docs)
 
  import userReducer from './user/user.reducer';
  import cartReducer from './cart/cart.reducer';
+ import directoryReducer from './directory/directory.reducer';
+ import shopReducer from './shop/shop.reducer';
 
- export default combineReducers({
-   user: userReducer,
-   cart: cartReducer,
- });
+ // config
+ const persistConfig = {
+   key: 'root',
+   storage,
+   whitelist: ['cart'] // list of reducers - not persisting user because firebase is handling it
+ } 
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer,
+});
+
+ export default persistReducer(persistConfig, rootReducer);
